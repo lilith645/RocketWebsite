fc61be8825ad6e6289e1ca2cfd462f721a0d3a3d
Lilith645 <lilith@inet-sys.com>
2020-08-21 18:27:47 +1000

    Initial Rocket commit with basic outlayer of website Changes committed: 	new file:   .gitignore 	new file:   Cargo.toml 	new file:   Rocket.toml 	new file:   games/Steel&Silver/2Icon.png 	new file:   games/Steel&Silver/Icon.png 	new file:   src/main.rs 	new file:   static/form-17a.pdf 	new file:   static/resume.pdf 	new file:   static/test.html 	new file:   templates/about.hbs 	new file:   templates/css/default.css 	new file:   templates/css/footer.css 	new file:   templates/css/grid.css 	new file:   templates/css/nav.css 	new file:   templates/error/404.hbs 	new file:   templates/footer.hbs 	new file:   templates/game.hbs 	new file:   templates/game_grid.hbs 	new file:   templates/hello.hbs 	new file:   templates/index.hbs 	new file:   templates/layout.hbs 	new file:   templates/nav.hbs

---
+++ src/main.rs
@@ -0,0 +1,143 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::response::NamedFile;
+use std::path::Path;
+use std::path::PathBuf;
+use rocket::http::RawStr;
+
+use rocket::response::{Flash, Redirect};
+use rocket::request::FlashMessage;
+use rocket::Request;
+
+use std::collections::HashMap;
+
+use rocket_contrib::templates::{Template, handlebars};
+
+use std::{fs, io};
+
+#[derive(serde::Serialize)]
+struct TemplateContext {
+    title: &'static str,
+    name: Option<String>,
+    items: Vec<String>,
+    // This key tells handlebars which template is the parent.
+    parent: &'static str,
+}
+
+#[get("/")]
+fn index() -> Template {
+  Template::render("about", &TemplateContext {
+      title: "About",
+      name: None,
+      items: vec![format!("Four"), format!("Five"), format!("Six")],
+      parent: "layout",
+  })
+}
+
+/*
+#[get("/")]
+fn index() -> Template {
+    let context = context();
+    Template::render("index", &context)
+}*//*
+#[get("/")]
+fn index() -> &'static str { // Template {
+  //let context = 
+  //Template::render("index", &context)
+  
+  //"Hello, world!"
+}*/
+
+#[get("/r")]
+fn redirect() -> Flash<Redirect> {
+  // Redirect::to(uri!(index))
+  Flash::success(Redirect::to(uri!(flash)), "Successfully logged out.")
+}
+
+#[get("/flash")]
+fn flash(flash: FlashMessage) -> String {
+  
+  //  "Hello, world!"
+  let msg = flash.msg();
+  
+  msg.to_string()
+}
+
+#[get("/hello/<name>")]
+fn hello(name: &RawStr) -> Template {//-> String {
+  let mut map = std::collections::HashMap::new();
+  map.insert("name", name.as_str());
+  Template::render("hello", &map)
+}
+
+#[get("/games")]
+fn games() -> Template {
+  let mut entries = fs::read_dir("./games/").unwrap()
+        .map(|res| res.map(|e| e.path().to_str().unwrap().to_string().split_off(8)))
+        .collect::<Result<Vec<_>, io::Error>>().unwrap();
+  println!("{:?}", entries);
+  
+  Template::render("game_grid", &TemplateContext {
+    title: "Games",
+    name: None,
+    items: entries,
+    parent: "layout",
+  })
+}
+
+#[get("/resume")]
+fn resume() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/resume.pdf")).ok()
+}
+
+#[get("/files/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+#[get("/css/<file..>")]
+fn css(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("templates/css/").join(file)).ok()
+}
+
+#[get("/games/<file..>")]
+fn game_images(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("games/").join(file)).ok()
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+  let mut map = std::collections::HashMap::new();
+  map.insert("path", req.uri().path());
+  Template::render("error/404", &map)
+}
+
+use self::handlebars::{Helper, Handlebars, Context, RenderContext, Output, HelperResult, JsonRender};
+
+fn wow_helper(
+    h: &Helper<'_, '_>,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext<'_>,
+    out: &mut dyn Output
+) -> HelperResult {
+    if let Some(param) = h.param(0) {
+        out.write("<b><i>")?;
+        out.write(&param.value().render())?;
+        out.write("</b></i>")?;
+    }
+
+    Ok(())
+}
+
+fn main() {
+    rocket::ignite()
+      .mount("/", routes![index, redirect, flash, hello, games, game_images, resume, files, css])
+      .register(catchers![not_found])
+      .attach(Template::custom(|engines| {
+            engines.handlebars.register_helper("wow", Box::new(wow_helper));
+        }))
+      .launch();
+}
